5856a8457acd9a4793755b60c020c4a0f24819e6
uswah <uswaaalvi@gmail.com>
2024-06-29 14:49:17 +0500

    tabs formed

--- app/src/main/java/com/example/salessync/Home.java
+++ app/src/main/java/com/example/salessync/Home.java
@@ -1,40 +1,45 @@
 package com.example.salessync;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import java.util.Objects;
+
 public class Home extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     TabAdapter tabAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init();
         viewPager2.setAdapter(tabAdapter);
 
-        TabLayoutMediator TLM=new TabLayoutMediator(
+        TabLayoutMediator TLM = new TabLayoutMediator(
                 tabLayout,
                 viewPager2,
                 new TabLayoutMediator.TabConfigurationStrategy() {
                     @Override
-                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int i) {
-                        switch (i){
-                            case 1:
-                                tab.setText("Inventory");
-                                break;
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch (position) {
                             case 0:
                                 tab.setText("Sales");
+                                break;
+                            case 1:
+                                tab.setText("Inventory");
                                 break;
                         }
                     }
@@ -44,8 +49,8 @@ public class Home extends AppCompatActivity {
 
     }
 
-    private void init(){
-        EdgeToEdge.enable(this);
+
+    private void init() {
         setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -56,5 +61,4 @@ public class Home extends AppCompatActivity {
         viewPager2 = findViewById(R.id.viewPager2);
         tabAdapter = new TabAdapter(this);
     }
-
 }
